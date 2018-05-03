cd628f3d16842f2c55ae8bb9ce475d466fdca627
Janek Laudan <laudan@tu-berlin.de>
2018-05-03 12:25:23 +0200

    *  Abstracted text input and error message into components *  Also added a modal component

---
+++ src/components/Error.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .error {{message}}
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: ['message'],
+})
+</script>
+
+<style scoped>
+.error {
+  color: white;
+  background-color: #ef5350;
+  border-radius: 4px;
+  padding: 0.5rem 0.5rem 0.5rem 1rem;
+}
+</style>
+
---
+++ src/project/CreateVisualization.vue
@@ -0,0 +1,68 @@
+<template lang="pug">
+modal(v-on:close-requested="cancel()")
+    span(slot="header") Create Visualization
+    div(slot="content")
+        text-input(label="Pick a network file" v-model="foo")
+        error(v-if="isServerError" v-bind:message="serverError")
+    div(slot="actions")
+        button.ui.negative.button(v-on:click="cancel()") Cancel
+        button.ui.positive.button(v-on:click="createVisualization()") Create
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import TextInput from '@/components/TextInput.vue'
+import Modal from '@/components/Modal.vue'
+import Error from '@/components/Error.vue'
+import { setTimeout } from 'timers'
+import FileAPI from '../communication/FileAPI'
+import { CreateVisualizationRequest } from '../entities/Visualization'
+
+interface CreateVisualizationState {
+  isRequesting: boolean
+  foo: string
+  request: CreateVisualizationRequest
+}
+
+export default Vue.extend({
+  components: {
+    modal: Modal,
+    'text-input': TextInput,
+    error: Error,
+  },
+  props: {
+    project: Object,
+  },
+  data(): any {
+    return {
+      isRequesting: false,
+      isServerError: false,
+      serverError: '',
+      foo: 'bar',
+      request: {
+        projectId: this.project.id,
+        typeKey: 'raw-files',
+        inputFiles: {},
+        inputPrameters: {},
+      },
+    }
+  },
+  methods: {
+    cancel: function() {
+      this.$emit('close')
+    },
+    createVisualization: async function() {
+      this.isRequesting = true
+      try {
+        let answer = await FileAPI.createVisualization({})
+      } catch (error) {
+        this.isServerError = true
+        this.serverError = error.message
+      } finally {
+        this.isRequesting = false
+      }
+    },
+  },
+})
+</script>
+
