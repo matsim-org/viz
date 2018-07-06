2044c7550e6002ff8f2d6eeb8ef454e3bb07201c
Billy Charlton <billy@okbecause.com>
2018-07-06 13:44:16 +0200

    fix merge: handleProject

--- src/project/Projects.vue
+++ src/project/Projects.vue
@@ -5,7 +5,7 @@
     div.emptyMessage(v-if="sharedState.personalProjects.length === 0")
       span No projects yet. Create one!
     .projectList(v-else)
-        button.projectItem(v-for="project in sharedState.personalProjects" v-on:click="onProjectClicked(project.id)")
+        button.projectItem(v-for="project in sharedState.personalProjects" v-on:click="handleProjectClicked(project.id)")
           list-element(v-bind:key="project.id")
             span(slot="title") {{project.name}}
             span(slot="content") {{project.id}}
