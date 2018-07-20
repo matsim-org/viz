e21efac372af36bd20ba5bd0eb54df440c27bd73
Janek Laudan <laudan@tu-berlin.de>
2018-07-20 12:28:08 +0200

    Add worker-loader to configuration *  set up Worker-loader for js and ts files

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.worker\.js$/,
+          use: [{ loader: 'worker-loader' }, { loader: 'babel-loader' }],
+        },
+
+        {
+          test: /\.worker\.ts$/,
+          use: [{ loader: 'worker-loader' }, { loader: 'babel-loader' }, { loader: 'ts-loader' }],
+        },
+      ],
+    },
+  },
+}
