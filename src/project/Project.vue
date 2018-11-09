fa9a91f1c3b796cede564332ab96b54db2b3c2bf
Billy Charlton <charlton@tu-berlin.de>
2018-11-09 18:36:44 +0100

    add drag/drop for files

--- src/project/Project.vue
+++ src/project/Project.vue
@@ -27,28 +27,157 @@
         multiple
         v-on:change="onFileInputChanged"
         )
-    .files
-      .emptyMessage(v-if="project.files && project.files.length === 0")
-        span No files yet. Add some!
-      .fileList(v-else)
-        .fileItem(v-for="file in project.files")
-          list-element( v-bind:key="file.id" v-on:itemClicked="handleFileClicked(file.id)")
-            .itemTitle(slot="title")
-              span {{file.userFileName}}
-              span {{readableFileSize(file.sizeInBytes)}}
+    .file-area
+      drop.drop(
+        :class="{over}"
+        @dragover="over = true"
+        @dragleave="over = false"
+        @drop="handleDrop"
+        effect-allowed='all'
+      ): b Drag/drop files here!
 
-            span(slot="content") {{file.id}}
+      .files
+        .emptyMessage(v-if="project.files && project.files.length === 0")
+          b No files yet. Add some!
+        .fileList(v-else)
+          .fileItem(v-for="file in project.files")
+            list-element( v-bind:key="file.id" v-on:itemClicked="handleFileClicked(file.id)")
+              .itemTitle(slot="title")
+                span {{file.userFileName}}
+                span {{readableFileSize(file.sizeInBytes)}}
 
-            button.button.is-small.is-rounded.is-warning(slot="accessory" v-on:click="handleDeleteFileClicked(file.id)") Delete
+              span(slot="content") {{file.id}}
+
+              button.button.is-small.is-rounded.is-warning(slot="accessory" v-on:click="handleDeleteFileClicked(file.id)") Delete
 
   create-visualization(v-if="showCreateVisualization"
                         v-on:close="handleAddVisualizationClosed"
                         v-bind:project="project")
 </template>
+<script lang="ts">
+import Vue from 'vue'
+import CreateVisualization from '@/project/CreateVisualization.vue'
+import ListHeader from '@/components/ListHeader.vue'
+import ListElement from '@/components/ListElement.vue'
+import Modal from '@/components/Modal.vue'
+import SharedStore, { SharedState } from '@/SharedStore'
+import Project from '@/entities/Project'
+import VizThumbnail from '@/components/VizThumbnail.vue'
+import { Visualization } from '@/entities/Visualization'
+import FileAPI from '@/communication/FileAPI'
+import { File } from 'babel-types'
+import filesize from 'filesize'
+import { Drag, Drop } from 'vue-drag-drop'
 
+interface ProjectState {
+  sharedState: SharedState
+  project: Project
+  isFetchingData: boolean
+  showCreateVisualization: boolean
+  over: boolean
+}
+
+export default Vue.extend({
+  components: {
+    'create-visualization': CreateVisualization,
+    'list-header': ListHeader,
+    'list-element': ListElement,
+    'viz-thumbnail': VizThumbnail,
+    Drag,
+    Drop,
+  },
+  data(): ProjectState {
+    return {
+      sharedState: SharedStore.state,
+      project: {
+        id: this.$route.params.projectId,
+        name: '',
+        files: [],
+        creator: {},
+        visualizations: [],
+      },
+      isFetchingData: false,
+      showCreateVisualization: false,
+      over: false,
+    }
+  },
+  created: async function(): Promise<void> {
+    const project = this.sharedState.personalProjects.find(element => element.id === this.project.id)
+    if (project) {
+      this.project = project
+    }
+
+    if (!project || !project.files) {
+      this.isFetchingData = true
+      this.project = await FileAPI.fetchProject(this.project.id)
+      this.isFetchingData = false
+    }
+  },
+  computed: {
+    projectId: function(): string {
+      return this.$route.params.id
+    },
+  },
+  methods: {
+    readableFileSize: function(bytes: number): string {
+      return filesize(bytes)
+    },
+    handleAddFileClicked: function(): void {
+      const input = this.$refs.fileInput as HTMLInputElement
+      input.click()
+    },
+    handleFileClicked: async function(fileId: string): Promise<void> {
+      try {
+        const blob = await FileAPI.downloadFile(fileId, this.project.id)
+        window.open(URL.createObjectURL(blob))
+      } catch (e) {
+        console.log(e)
+      }
+    },
+    handleDeleteFileClicked: async function(fileId: string): Promise<void> {
+      try {
+        this.project = await FileAPI.deleteFile(fileId, this.project)
+      } catch (e) {
+        console.log(e)
+      }
+    },
+    handleAddVisualizationClicked: function(): void {
+      this.showCreateVisualization = true
+    },
+    handleAddVisualizationClosed: function(visualization: Visualization): void {
+      this.showCreateVisualization = false
+      if (visualization) this.project.visualizations.push(visualization)
+    },
+    handleVisualizationClicked: function(viz: Visualization): void {
+      this.$router.push({ path: `/${viz.type}/${this.project.id}/${viz.id}` })
+    },
+    onFileInputChanged: async function(): Promise<void> {
+      const files = (this.$refs.fileInput as any).files
+      try {
+        const updatedProject = await FileAPI.uploadFiles(files, this.project)
+        this.project = updatedProject
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    handleDrop: async function(data: any, event: any) {
+      event.preventDefault()
+      this.over = false
+      const files = event.dataTransfer.files
+
+      try {
+        const updatedProject = await FileAPI.uploadFiles(files, this.project)
+        this.project = updatedProject
+      } catch (error) {
+        console.error(error)
+      }
+    },
+  },
+})
+</script>
 <style scoped>
 section {
-  margin: 4rem 1.5rem 4rem 1.5rem;
+  margin: 4rem 1.5rem 2rem 1.5rem;
 }
 
 .project {
@@ -127,110 +256,30 @@ section {
 .viz-item:hover {
   cursor: pointer;
 }
-</style>
-
-<script lang="ts">
-import Vue from 'vue'
-import CreateVisualization from '@/project/CreateVisualization.vue'
-import ListHeader from '@/components/ListHeader.vue'
-import ListElement from '@/components/ListElement.vue'
-import Modal from '@/components/Modal.vue'
-import SharedStore, { SharedState } from '@/SharedStore'
-import Project from '@/entities/Project'
-import VizThumbnail from '@/components/VizThumbnail.vue'
-import { Visualization } from '@/entities/Visualization'
-import FileAPI from '@/communication/FileAPI'
-import { File } from 'babel-types'
-import filesize from 'filesize'
-
-interface ProjectState {
-  sharedState: SharedState
-  project: Project
-  isFetchingData: boolean
-  showCreateVisualization: boolean
+.drop {
+  padding: 25px;
+  margin: 20px 10px 50px 0px;
+  text-align: center;
+  border: 5px dashed #ddd;
+  border-radius: 10px;
 }
 
-export default Vue.extend({
-  components: {
-    'create-visualization': CreateVisualization,
-    'list-header': ListHeader,
-    'list-element': ListElement,
-    'viz-thumbnail': VizThumbnail,
-    modal: Modal,
-  },
-  data(): ProjectState {
-    return {
-      sharedState: SharedStore.state,
-      project: {
-        id: this.$route.params.projectId,
-        name: '',
-        files: [],
-        creator: {},
-        visualizations: [],
-      },
-      isFetchingData: false,
-      showCreateVisualization: false,
-    }
-  },
-  created: async function(): Promise<void> {
-    const project = this.sharedState.personalProjects.find(element => element.id === this.project.id)
-    if (project) {
-      this.project = project
-    }
+.drop:hover {
+  border: 5px dashed pink;
+}
 
-    if (!project || !project.files) {
-      this.isFetchingData = true
-      this.project = await FileAPI.fetchProject(this.project.id)
-      this.isFetchingData = false
-    }
-  },
-  computed: {
-    projectId: function(): string {
-      return this.$route.params.id
-    },
-  },
-  methods: {
-    readableFileSize: function(bytes: number): string {
-      return filesize(bytes)
-    },
-    handleAddFileClicked: function(): void {
-      const input = this.$refs.fileInput as HTMLInputElement
-      input.click()
-    },
-    handleFileClicked: async function(fileId: string): Promise<void> {
-      try {
-        const blob = await FileAPI.downloadFile(fileId, this.project.id)
-        window.open(URL.createObjectURL(blob))
-      } catch (e) {
-        console.log(e)
-      }
-    },
-    handleDeleteFileClicked: async function(fileId: string): Promise<void> {
-      try {
-        this.project = await FileAPI.deleteFile(fileId, this.project)
-      } catch (e) {
-        console.log(e)
-      }
-    },
-    handleAddVisualizationClicked: function(): void {
-      this.showCreateVisualization = true
-    },
-    handleAddVisualizationClosed: function(visualization: Visualization): void {
-      this.showCreateVisualization = false
-      if (visualization) this.project.visualizations.push(visualization)
-    },
-    handleVisualizationClicked: function(viz: Visualization): void {
-      this.$router.push({ path: `/${viz.type}/${this.project.id}/${viz.id}` })
-    },
-    onFileInputChanged: async function(): Promise<void> {
-      const files = (this.$refs.fileInput as any).files
-      try {
-        const updatedProject = await FileAPI.uploadFiles(files, this.project)
-        this.project = updatedProject
-      } catch (error) {
-        console.error(error)
-      }
-    },
-  },
-})
-</script>
+.drop.over {
+  border: 5px dashed blue;
+  background-color: #ccc;
+}
+
+.file-area {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: 1fr;
+}
+
+.files {
+  margin-left: 10px;
+}
+</style>
