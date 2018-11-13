85460c4a8ae9ced96ff4942d89d55d8623feb08a
Janek <j.laudan@mailbox.org>
2018-11-13 15:55:30 +0100

    Start creating a file upload

---
+++ src/project/FileUpload.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+modal(v-on:close-requested="close()")
+  span(slot="header")
+    h3 Upload Files
+  
+  div(slot="content")
+    aside.menu
+      p Things to set up
+    .main-content
+      p here will be main content
+    
+  div(slot="actions")
+    button.ui.negative.button(v-on:click="close()") Cancel
+    
+
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Modal from '@/components/Modal.vue'
+
+export default Vue.extend({
+  components: {
+    modal: Modal,
+  },
+  methods: {
+    close: function(): void {
+      this.$emit('close')
+    },
+  },
+})
+</script>
