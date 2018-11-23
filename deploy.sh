767bfc360c62969b866f175736d05dace9bf9feb
Jakub Wilk <jwilk79@users.noreply.github.com>
2018-11-23 11:53:09 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 # VSP deploy script for travis CI
 eval $(ssh-agent -s)
 # see bug with line endings at https://gitlab.com/gitlab-examples/ssh-private-key/issues/1
-# this works and needs a base64 -w0 encoded key with "cat id_rsa | base64 -w0"
+# this works and needs a base64 -w0 encoded key with cmd "cat id_rsa | base64 -w0"
+# and the content of var VSP_SSH_KEY_BASE64 to be in double-quotes (in Travis)
 #ssh-add <(echo "$VSP_SSH_KEY_BASE64"| base64 --decode)
 # another approach: convert line endings on the fly
 echo "$VSP_SSH_KEY" | dos2unix | ssh-add -
