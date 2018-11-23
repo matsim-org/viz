ba2e9106e3f4bb09f42d1434d2dde1a2418d6918
Jakub Wilk <jwilk79@users.noreply.github.com>
2018-11-23 11:47:56 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,10 @@
 # VSP deploy script for travis CI
 eval $(ssh-agent -s)
 # see bug with line endings at https://gitlab.com/gitlab-examples/ssh-private-key/issues/1
-# this needs a base64 -w0 encoded key
-ssh-add <(echo "$VSP_SSH_KEY"| base64 --decode)
+# this works and needs a base64 -w0 encoded key with "cat id_rsa | base64 -w0"
+#ssh-add <(echo "$VSP_SSH_KEY_BASE64"| base64 --decode)
+# another approach: convert line endings on the fly
+echo "$VSP_SSH_KEY" | dos2unix | ssh-add -
 # remove old webfiles
 ssh vizdeploy@viz.vsp.tu-berlin.de 'rm -r /var/www/viz-dev/*'
 # upload fresh built files
