ed8adc5752345b617e22b182eb879c37286887b4
Janek Laudan <laudan@tu-berlin.de>
2018-11-23 16:29:16 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ ssh-add <(echo "$VSP_SSH_KEY_BASE64"| base64 --decode)
 #echo "$VSP_SSH_KEY" | dos2unix | ssh-add -
 #echo "${VSP_SSH_KEY}" | tr -d '\r' | ssh-add - > /dev/null
 # remove old webfiles
-ssh vizdeploy@viz.vsp.tu-berlin.de 'rm -r /var/www/$DOMAIN/*'
+ssh vizdeploy@viz.vsp.tu-berlin.de 'rm -r /var/www/"$DOMAIN"/*'
 # upload fresh built files
-scp -r dist/* vizdeploy@viz.vsp.tu-berlin.de:/var/www/$DOMAIN/
+scp -r dist/* vizdeploy@viz.vsp.tu-berlin.de:/var/www/"$DOMAIN"/
