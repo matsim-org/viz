3c94d11b2d80fb8248a7c70e3db0f94fb28b41a8
Billy Charlton <charlton@tu-berlin.de>
2018-12-20 18:18:38 +0100

    update babel,vue-cli packages for better debugging support

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,17 @@
 module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'vue'],
+
   transform: {
     '^.+\\.vue$': 'vue-jest',
-    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.js$': 'babel-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
   },
+
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+
   snapshotSerializers: ['jest-serializer-vue'],
   testMatch: ['**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'],
   setupFiles: ['jest-localstorage-mock', '<rootDir>/tests/unit/setup'],
