b37a84453107707e6fba30c3666c837f6c9d1452
bc <charlton@tu-berlin.de>
2019-09-30 18:25:27 +0200

    fix #161: try to solve Webpack OOM crashes with more RAM for V8
    - the worker-loader was also doing extra conversions for no reason

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const os = require('os')
 const path = require('path')
 
 module.exports = {
-  parallel: false,
+  parallel: true,
   productionSourceMap: false,
   chainWebpack: config => {
     /*
@@ -12,37 +11,20 @@ module.exports = {
      */
     config.module.rule('js').exclude.add(/\.worker\.js$/)
     config.module.rule('ts').exclude.add(/\.worker\.ts$/)
-
-    config.plugin('fork-ts-checker').tap(args => {
-      let totalmem = Math.floor(os.totalmem() / 1024 / 1024) //get OS mem size
-      let allowUseMem = totalmem > 4096 ? 4096 : 2048
-      args[0].memoryLimit = allowUseMem
-      return args
-    })
   },
   configureWebpack: {
-    /*
-     * Setting the global object is necessary to make the loading of web-workers work. Otherwise, window is referenced
-     * in the produced bundle, which is not accessable from a worker context. This seems to be a webpack 4 bug
-     * https://github.com/webpack/webpack/issues/6642
-     * https://github.com/webpack/webpack/issues/6629
-     */
-    output: {
-      globalObject: 'self',
-      pathinfo: false,
-    },
     // devtool: 'source-map',
     module: {
       rules: [
         {
           test: /\.worker\.js$/,
           include: path.resolve(__dirname, 'src'),
-          use: [{ loader: 'worker-loader' }, { loader: 'babel-loader' }],
+          use: [{ loader: 'worker-loader' }],
         },
         {
           test: /\.worker\.ts$/,
           include: path.resolve(__dirname, 'src'),
-          use: [{ loader: 'worker-loader' }, { loader: 'babel-loader' }, { loader: 'ts-loader' }],
+          use: [{ loader: 'worker-loader' }, { loader: 'ts-loader' }],
         },
       ],
     },
